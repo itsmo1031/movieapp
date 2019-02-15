e92f2f07e9f6b3afe702d5c52c587865fd81f0fe
itsmo1031 <itsmo1031@gmail.com>
2019-02-15 11:39:35 -0800

    Done #5. Stateless Functional Components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Movie from "./Movie";
 
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDiDMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate(): boolean(true/false) ->
+  //         componentWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {};
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          {
+            title: "Coco",
+            poster:
+              "https://www.slashfilm.com/wp/wp-content/images/2017-bestposter-coco.jpg"
+          },
+          {
+            title: "Frozen 2",
+            poster:
+              "https://m.media-amazon.com/images/M/MV5BODYwMzYzODEzNV5BMl5BanBnXkFtZTgwOTgxOTE0NzM@._V1_.jpg"
+          },
+          {
+            title: "Avengers: Endgame",
+            poster:
+              "https://m.media-amazon.com/images/M/MV5BNGZiMzBkZjMtNjE3Mi00MWNlLWIyYjItYTk3MjY0Yjg5ODZkXkEyXkFqcGdeQXVyNDg4NjY5OTQ@._V1_SY1000_CR0,0,675,1000_AL_.jpg"
+          },
+          {
+            title: "Spider-Man: Into the Spider-Verse",
+            poster:
+              "https://m.media-amazon.com/images/M/MV5BMjMwNDkxMTgzOF5BMl5BanBnXkFtZTgwNTkwNTQ3NjM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+          },
+          {
+            title: "Spider-Man: Far From Home",
+            poster:
+              "https://m.media-amazon.com/images/M/MV5BMjE1MzE5MjczMl5BMl5BanBnXkFtZTgwOTgwMTgxNzM@._V1_SY1000_CR0,0,674,1000_AL_.jpg"
+          }
+        ]
+      });
+    }, 5000);
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />;
+    });
+    return movies;
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello!</p>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        {this.state.movies ? this._renderMovies() : "Loading"}
       </div>
     );
   }
---
+++ src/Movie.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import "./Movie.css";
+
+// class Movie extends Component {
+//   static propTypes = {
+//     title: PropTypes.string.isRequired,
+//     poster: PropTypes.string
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <MoviePoster poster={this.props.poster} />
+//         <h1>{this.props.title}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+// Turn into the FUNCTIONAL COMPONENT
+function Movie({ title, poster }) {
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
+}
+
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+};
+
+// class MoviePoster extends Component {
+//   static propTypes = {
+//     poster: PropTypes.string.isRequired
+//   };
+
+//   render() {
+//     return <img src={this.props.poster} alt="Movie Poster" />;
+//   }
+// }
+
+// Turn into the FUNCTIONAL COMPONENT
+function MoviePoster({ poster }) {
+  return <img src={poster} alt="Movie Poster" />;
+}
+
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
+};
+
+export default Movie;
