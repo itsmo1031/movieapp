67e607c01c92d8c32075ef48bfcb2b5ad5041101
itsmo1031 <itsmo1031@gmail.com>
2019-02-18 16:42:18 -0800

    CSS Applied

--- src/Movie.js
+++ src/Movie.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import "./Movie.css";
+import LinesEllipsis from "react-lines-ellipsis";
 
 // class Movie extends Component {
 //   static propTypes = {
@@ -22,17 +23,25 @@ import "./Movie.css";
 function Movie({ title, poster, genres, synopsis }) {
   return (
     <div className="Movie">
-      <div className="Movie__Columns">
+      <div className="Movie__Column">
         <MoviePoster poster={poster} alt={title} />
       </div>
-      <div className="Movie__Columns">
+      <div className="Movie__Column">
         <h1>{title}</h1>
         <div className="Movie__Genres">
           {genres.map((genre, index) => (
             <MovieGenre genre={genre} key={index} />
           ))}
         </div>
-        <p className="Movie__Synopsis">{synopsis}</p>
+        <div className="Movie__Synopsis">
+          <LinesEllipsis
+            text={synopsis}
+            maxLine="3"
+            ellipsis="..."
+            trimRight
+            basedOn="letters"
+          />
+        </div>
       </div>
     </div>
   );
@@ -45,7 +54,7 @@ function MovieGenre({ genre }) {
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
   poster: PropTypes.string.isRequired,
-  genres: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
   synopsis: PropTypes.string.isRequired
 };
 
