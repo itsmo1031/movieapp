57b9bd4d69e6e66a36eb9cc9e3991b2ea9a9eeed
itsmo1031 <itsmo1031@gmail.com>
2019-02-18 16:51:06 -0800

    Updates

---
+++ precache-manifest.ba9af11b5626985e9afce0df40d5f0a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c952458630975ff1f4a7",
+    "url": "/movieapp/static/css/main.a453e494.chunk.css"
+  },
+  {
+    "revision": "c952458630975ff1f4a7",
+    "url": "/movieapp/static/js/main.c9524586.chunk.js"
+  },
+  {
+    "revision": "063273b0e9fe9ddfee2c",
+    "url": "/movieapp/static/js/runtime~main.063273b0.js"
+  },
+  {
+    "revision": "8ace2eaf1c51223fac7b",
+    "url": "/movieapp/static/js/2.8ace2eaf.chunk.js"
+  },
+  {
+    "revision": "0396c8201bde8ea1c299293266bb6d87",
+    "url": "/movieapp/index.html"
+  }
+];
